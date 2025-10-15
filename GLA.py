f1723b8d82e14caaf35fcf827a62493f31c36282
kmance64 <karlo.mance@fer.unizg.hr>
2025-10-15 20:39:21 +0200

    Dodane funkcije za prosirivanje definicija i pravila.(uklonjeno 'nestanje')

--- GLA.py
+++ GLA.py
@@ -56,12 +56,42 @@ def parsiranje():
                     "akcije": akcije
                 })
         i += 1
-
     return regularne_definicije, stanja, uniformni_znakovi, pravila
 
+def prosiri_definicije(definicije):
+    promijenjeno = True  # Kada se nista u definicija ne promijeni znaci da je sve promijenjeno
+    while promijenjeno:
+        promijenjeno = False
+        for ime, izraz in list(definicije.items()): #list radi kopiju definicija - inace dolazi do greske jer mijenjam rječnik kroz koji iteriram
+            novi_izraz = izraz
+            matches = re.findall(r"\{([^}]+)\}", izraz) # vraca listu svih matcheva
+            for m in matches:
+                if m in definicije:
+                    novi_izraz = novi_izraz.replace(f"{{{m}}}", f"({definicije[m]})") #replace i dodajem zagrade oko izraza da se ne promijeni redoslijed
+                    promijenjeno = True #oznacim da se nesto promijenilo tako da opet prolazim kroz sve definicije
+                definicije[ime] = novi_izraz
+    return definicije
+
+def prosiri_pravila(pravila, definicije): #prosljedujem prosirene definicije
+    for pravilo in pravila:
+        regex = pravilo["regex"] # pravilo izgleda ovako: {'stanje': 'S_a', 'regex': '\\n', 'akcije': ['-', 'NOVI_REDAK']}
+        matches = re.findall(r"\{([^}]+)\}", regex)
+        for m in matches:
+            if m in definicije:
+                regex = regex.replace(f"{{{m}}}", f"({definicije[m]})")
+        pravilo["regex"] = regex
+    return pravila
 
 if __name__ == "__main__":
     defs, stanja, u_znakovi, pravila = parsiranje()
+    print("DEF: ", defs)
+    print("STANJA: ", stanja)
+    print("U_ZNAKOVI: ", u_znakovi)
+    print("PRAVILA: ", pravila)
+
+    defs = prosiri_definicije(defs)
+    pravila = prosiri_pravila(pravila, defs)
+
     print("DEF: ", defs)
     print("STANJA: ", stanja)
     print("U_ZNAKOVI: ", u_znakovi)
